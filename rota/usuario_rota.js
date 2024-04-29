065094d68f5299bf3cd80693104a932b9f269b7d
Paola Fantinel <fantinelpaola15@gmail.com>
2024-04-29 19:39:14 -0300

    Nova correção das rotas

--- rota/usuario_rota.js
+++ rota/usuario_rota.js
@@ -6,9 +6,9 @@ const router = express.Router()
 
 router.post('/', controller.addUsuario)
 router.get('/', controller.buscarUsuario)
-router.get('/:nome', controller.buscarUsuarioNome)
+router.get('/nome/:nome', controller.buscarUsuarioNome)
 router.get('/:id', controller.buscarUsuarioId)
-router.get('/:email', controller.buscarUsuarioEmail)
+router.get('/email/:email', controller.buscarUsuarioEmail)
 router.put('/:id', controller.atualizarUsuario)
 router.delete('/:id', controller.deletarUsuario)
 
