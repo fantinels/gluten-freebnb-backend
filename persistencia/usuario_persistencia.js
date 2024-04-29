f2a7d7a1a29355c1345b9072ffe63d33ade35a14
Paola Fantinel <fantinelpaola15@gmail.com>
2024-04-29 19:18:32 -0300

    CRUD usuário (feito para testes da API)

--- persistencia/usuario_persistencia.js
+++ persistencia/usuario_persistencia.js
@@ -1,12 +1,8 @@
-// const { Client } = require('pg')
-// const { conexao } = require('./conexao')
 const connect = require("../db");
 
 // Create
 async function addUsuario(usuario) {
     const client = await connect()
-    // const client = new Client(conexao)
-    // await client.connect()
 
     try {
         const sql = `INSERT INTO usuario(nome, cpf, email, telefone, cidade, uf, dt_nascimento, senha) 
@@ -14,8 +10,7 @@ async function addUsuario(usuario) {
         const values = [usuario.nome, usuario.cpf, usuario.email, usuario.telefone, usuario.cidade, usuario.uf, usuario.dt_nascimento, usuario.senha]
         const usuarios = await client.query(sql, values)
 
-        // await client.end()
-        await client.release
+        client.release
         return usuarios.rows[0]
     } catch (error) { throw error }
 }
@@ -23,48 +18,89 @@ async function addUsuario(usuario) {
 // Read
 async function buscarUsuario() {
     const client = await connect()
-    // const client = new Client(conexao)
-    // await client.connect()
 
     try {
         const sql = `SELECT * FROM usuario`
         const usuarios = await client.query(sql)
-        await client.end()
-        // await client.release
+
+        client.release
         return usuarios.rows
     } catch (error) { throw error }
 }
 
 async function buscarUsuarioNome(nome) {
     const client = await connect()
-    // const client = new Client(conexao)
-    // await client.connect()
+
+    try {
+        const sql = `SELECT * FROM usuario WHERE nome = $1`
+        const values = [nome]
+        const usuarios = await client.query(sql, values)
+
+        client.release
+        return usuarios.rows[0]
+    } catch (error) { throw error }
 }
 
 async function buscarUsuarioId(id) {
     const client = await connect()
-    // const client = new Client(conexao)
-    // await client.connect()
+
+    try {
+        const sql = `SELECT * FROM usuario WHERE id = $1`
+        const values = [id]
+        const usuarios = await client.query(sql, values)
+
+        client.release
+        return usuarios.rows[0]
+    } catch (error) { throw error }
 }
 
-async function buscarUsuarioEmail(id) {
+async function buscarUsuarioEmail(email) {
     const client = await connect()
-    // const client = new Client(conexao)
-    // await client.connect()
+
+    try {
+        const sql = `SELECT * FROM usuario WHERE email = $1`
+        const values = [email]
+        const usuarios = await client.query(sql, values)
+
+        client.release
+        return usuarios.rows[0]
+    } catch (error) { throw error }
 }
 
 // Update
-async function atualizarUsuario(id, alunos) {
+async function atualizarUsuario(id, usuario) {
     const client = await connect()
-    // const client = new Client(conexao)
-    // await client.connect()
+
+    try {
+        const sql = `UPDATE usuario SET nome          = $1,
+                                        cpf           = $2,
+                                        email         = $3,
+                                        telefone      = $4,
+                                        cidade        = $5,
+                                        uf            = $6,
+                                        dt_nascimento = $7,
+                                        senha         = $8
+                      WHERE id = $1 RETURNING *`
+        const values = [usuario.nome, usuario.cpf, usuario.email, usuario.telefone, usuario.cidade, usuario.uf, usuario.dt_nascimento, usuario.senha]
+        const usuarios = await client.query(sql, values)
+
+        client.release
+        return usuarios.rows[0]
+    } catch (error) { throw error }
 }
 
 // Delete
 async function deletarUsuario(id) {
     const client = await connect()
-    // const client = new Client(conexao)
-    // await client.connect()
+
+    try {
+        const sql = `DELETE FROM usuario WHERE id = $1`
+        const values = [id]
+        const usuarios = await client.query(sql, values)
+
+        client.release
+        return usuarios.rows[0]
+    } catch (error) { throw error }
 }
 
 module.exports = {
