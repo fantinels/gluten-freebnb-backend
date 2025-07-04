794bbcf0723a2ac9e846fa359d257d99183e6bfe
Gui-Menezes <64746108+Gui-Menezes@users.noreply.github.com>
2025-07-04 16:25:29 -0300

    Incluído parâmetro ssl no objeto Pool

--- db.js
+++ db.js
@@ -6,6 +6,9 @@ async function connect() {
 
   const pool = new Pool({
     connectionString: process.env.CONNECTION_STRING,
+  ssl: {
+    rejectUnauthorized: false
+  }
   });
 
   const client = await pool.connect();
