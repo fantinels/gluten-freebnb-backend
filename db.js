59c2886a18a07bf31722a94c36ed8764745c4d8d
Gui-Menezes <64746108+Gui-Menezes@users.noreply.github.com>
2025-07-04 20:46:21 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,18 +1,26 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 
-const pool = new Pool({
-  connectionString: process.env.CONNECTION_STRING,
-  ssl: {
-    rejectUnauthorized: false
-  },
-  max: 1,
-  idleTimeoutMillis: 10000,
-  connectionTimeoutMillis: 5000,
-  keepAlive: true,
-});
+async function connect() {
+  const pool = new Pool({
+    connectionString: process.env.CONNECTION_STRING,
+    ssl: {
+      rejectUnauthorized: false
+    },
+    max: 1,
+    idleTimeoutMillis: 10000,
+    connectionTimeoutMillis: 5000,
+    keepAlive: true,
+  });
 
-module.exports = pool;
+  const client = await pool.connect();
+  client.release();
+  return pool.connect();
+  }
+
+module.exports = connect
+
+// module.exports = pool;
 
 // require('dotenv').config();
 
